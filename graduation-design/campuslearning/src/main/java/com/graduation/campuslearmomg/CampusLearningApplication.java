c2416a70e111d8f18d8bab7bd3f19b518ddfec4e
wenlikun <46392802+wenlikun@users.noreply.github.com>
2024-05-19 19:48:23 +0800

    first commit

---
+++ graduation-design/campuslearning/src/main/java/com/graduation/campuslearmomg/CampusLearningApplication.java
@@ -0,0 +1,21 @@
+package com.graduation.campuslearmomg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目名: graduation-design
+ * 创建者: Admin
+ * 创建时间: 2024/5/19
+ * 描述:
+ *
+ * @author ben-bird
+ */
+@SpringBootApplication
+public class CampusLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusLearningApplication.class,args);
+    }
+
+}
---
+++ graduation-design/campuslearning/src/main/java/com/graduation/campuslearmomg/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.graduation.campuslearmomg.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 项目名: design
+ * 创建者: Admin
+ * 创建时间: 2024/5/19
+ * 描述:
+ *
+ * @author ben-bird
+ */
+@RestController
+@RequestMapping("/campus")
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "campus-test";
+    }
+
+
+}
